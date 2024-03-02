501c033f707cea72882c9f5fe476357a6829a82a
hussein-elmlah <hussein.elmlah7@gmail.com>
2024-03-03 00:01:48 +0200

    update Lab2_Q1

--- Lab2/lab2-Q1.py
+++ Lab2/lab2-Q1.py
@@ -2,9 +2,9 @@ from abc import ABC, abstractmethod
 import math
 
 class Shape(ABC):
-    def __init__(self, name):
+    def __init__(self, name, color):
         self.name = name
-        self._color = None
+        self._color = color
     
     def set_color(self, color):
         self._color = color
@@ -17,24 +17,24 @@ class Shape(ABC):
         pass
 
 class Circle(Shape):
-    def __init__(self, name, radius):
-        super().__init__(name)
+    def __init__(self, name, color, radius):
+        super().__init__(name, color)
         self.radius = radius
     
     def calculate_area(self):
         return math.pi * self.radius**2
 
 class Square(Shape):
-    def __init__(self, name, side):
-        super().__init__(name)
+    def __init__(self, name, color, side):
+        super().__init__(name, color)
         self.side = side
     
     def calculate_area(self):
         return self.side**2
 
 class Rectangle(Shape):
-    def __init__(self, name, length, width):
-        super().__init__(name)
+    def __init__(self, name, color, length, width):
+        super().__init__(name, color)
         self.length = length
         self.width = width
     
@@ -42,9 +42,9 @@ class Rectangle(Shape):
         return self.length * self.width
 
 # Test the classes
-circle = Circle("Circle", 5)
-square = Square("Square", 5)
-rectangle = Rectangle("Rectangle", 5, 5)
+circle = Circle("Circle", "red", 5)
+square = Square("Square", "blue", 5)
+rectangle = Rectangle("Rectangle", "green", 5, 5)
 
 print("Area of Circle:", circle.calculate_area())
 print("Area of Square:", square.calculate_area())
